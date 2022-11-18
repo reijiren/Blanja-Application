8f4f2ddfd78c88715b4010a557b3ab15f93f0964
IchsanR <111603307+IchsanR@users.noreply.github.com>
2022-11-19 05:53:59 +0800

    revisi login register dan tambah font

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ScrollToTop from '../components/ScrollToTop';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop";
 
-import LoginCostumer from '../views/auth/loginCostumer';
-
-import LoginSeller from '../views/auth/loginSeller';
-import SignUpSeller from '../views/auth/signupSeller';
-import SignUpCostumer from '../views/auth/signupCustomer';
+import Login from "../pages/login/login";
+import Product from "../pages/product/product";
+import Register from "../pages/register/register";
 
 export default function Router() {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/"></Route>
-        <Route path="loginCostumer" element={<LoginCostumer />} />
-        <Route path="loginseller" element={<LoginSeller />} />
-        <Route path="signUpCostumer" element={<SignUpCostumer />} />
-        <Route path="signUpSeller" element={<SignUpSeller />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<ScrollToTop />
+			<Routes>
+				<Route path="/">
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Register />} />
+					<Route path="product" element={<Product />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
