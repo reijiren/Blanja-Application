e3d933d5f0f5837951ebbd2c972d820a51ce4097
IchsanR <111603307+IchsanR@users.noreply.github.com>
2022-11-21 17:34:02 +0800

    ..

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop";
 
+// admin
+import HomeAdmin from "../pages/admin/home/adminHome";
+
+// user dan seller
 import Login from "../pages/login/login";
 import Product from "../pages/product/product";
 import Register from "../pages/register/register";
@@ -15,6 +19,9 @@ export default function Router() {
 					<Route path="login" element={<Login />} />
 					<Route path="register" element={<Register />} />
 					<Route path="product" element={<Product />} />
+				</Route>
+				<Route path="/admin/">
+					<Route path="home" element={<HomeAdmin />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
