7e9e78f3edec8cb8536a9f486fbeceb070ba4e00
Iqbalapredo <iqbalapreedo@gmail.com>
2022-11-22 17:26:42 +0700

    update landing

--- frontend/src/pages/landing/landing.jsx
+++ frontend/src/pages/landing/landing.jsx
@@ -1,88 +1,104 @@
 import React from 'react';
-import Footer from '../../components/Footer/index';
-import Button from '../../components/Button/index';
-import Pic from '../../assets/images/1.png';
-import Pic2 from '../../assets/images/2.png';
-import Category from '../../components/CategoryCard/index';
-import Card from '../../components/Card/index';
+
 import styles from '../landing/style.module.css';
 import Navbar from '../../components/Navbar';
+import benjamin from '../../assets/images/benjamin-voros-TnNo84AJJ5A-unsplash 1.jpg';
+import benjanin from '../../assets/images/ian-dooley-10ca-K3e6Ko-unsplash 1.jpg';
+import card from '../../assets/images/bajuMuslim1.png';
+import product from '../../assets/images/bajuMuslim4.png';
 
 const Landing = () => {
   return (
     <>
       <Navbar />
-      <section className={styles.main}>
-        <section className={styles.polular}>
-          <section className={styles.caraousell}>
-            <div className="container">
-              <div className="row">
-                <div className={'col-12 d-flex ' + styles.caraousell}>
-                  <div>
-                    <img className={'img-fluid ' + styles.j1} src={Pic} alt="" />
-                  </div>
-                  <div>
-                    <img className={'img-fluid ' + styles.j2} src={Pic2} alt="" />
-                  </div>
-                  <div>
-                    <img className={'img-fluid ' + styles.j3} src={Pic} alt="" />
-                  </div>
-                  <div>
-                    <img className={'img-fluid ' + styles.j4} src={Pic2} alt="" />
-                  </div>
-                </div>
+      <section>
+        <div className="container">
+          <div className="d-flex justify-content-center mt-5">
+            <img className={styles.benjamin} src={benjamin} alt="" />
+            <img style={{ marginLeft: '15px' }} className={styles.benjamin} src={benjanin} alt="" />
+          </div>
+
+          <div className="row col-md-12">
+            <div className="ms-5 mt-4">
+              <h2>Category</h2>
+              <p>What are you currently looking for</p>
+            </div>
+          </div>
+
+          <div className="d-flex  ">
+            <div className={styles.bg}></div>
+            <div className={styles.bg1}></div>
+            <div className={styles.bg2}></div>
+            <div className={styles.bg3}></div>
+            <div className={styles.bg4}></div>
+          </div>
+          <div className="row col-md-12">
+            <div className="ms-5 mt-4">
+              <h2>New</h2>
+              <p>You’ve never seen it before!</p>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="card" style={{ width: '18rem', top: '20px' }}>
+              <img src={product} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-text">Men's formal suit - Black & White</h4>
+                <h4 className="card-text text-danger">$ 40.0</h4>
+                <p className={styles.p}>Zalora Cloth</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-1" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2  " viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <span className={`${styles.p} ms-2`}>(10)</span>
               </div>
             </div>
-          </section>
-          <h5>Category</h5>
-          <p className={styles.sub}>You've never seen it before</p>
-          <div className={styles.category}>
-            <Category />
           </div>
-          <div className={styles.maincontent}>
-            <h5>New</h5>
-            <p className={styles.sub}>You've never seen it before</p>
-            <div className={styles.sort}>
-              <select name="sortBy" id="sortBy">
-                <option value="">Sort Berdasarkan</option>
-                <option value="name">Nama</option>
-                <option value="price">Harga</option>
-              </select>
-              <select name="sort" id="sort">
-                <option value="asc">Terendah</option>
-                <option value="desc">Tertinggi</option>
-              </select>
-            </div>
 
-            <div className={styles.content}>
-              <Card />
-            </div>
-            <div className={styles.btnPagination}>
-              <Button title="1" className="pagination" />
-              <Button title="2" className="pagination" />
-              <Button title="3" className="pagination" />
-              <Button title="4" className="pagination" />
-              <Button title="5" className="pagination" />
+          <div className="row col-md-12">
+            <div className="ms-5 mt-5">
+              <h2>Popular</h2>
+              <p>Find clothes that are trending recently</p>
             </div>
           </div>
 
-          <div className={styles.maincontent}>
-            <h5>Popular</h5>
-            <p className={styles.sub}>You've never seen it before</p>
-            <div className={styles.content}>
-              <Card />
+          <div className="col-md-12">
+            <div className="card" style={{ width: '18rem', top: '20px' }}>
+              <img src={product} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h4 className="card-text">Men's formal suit - Black & White</h4>
+                <h4 className="card-text text-danger">$ 40.0</h4>
+                <p className={styles.p}>Zalora Cloth</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-1" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2  " viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star text-warning ms-2" viewBox="0 0 16 16">
+                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                </svg>
+                <span className={`${styles.p} ms-2`}>(10)</span>
+              </div>
             </div>
           </div>
-          <div className={styles.btnPagination}>
-            <Button title="1" className="pagination" />
-            <Button title="2" className="pagination" />
-            <Button title="3" className="pagination" />
-            <Button title="4" className="pagination" />
-            <Button title="5" className="pagination" />
-          </div>
-        </section>
-
-        <Footer />
+        </div>
       </section>
     </>
   );
