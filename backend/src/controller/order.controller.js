021c241b76580d3078c38bbff9ce63f1ea65ea96
lol <Rahmatfurqon0@gmail.com>
2022-11-22 16:58:40 +0700

    update BE transaction

--- backend/src/controller/order.controller.js
+++ backend/src/controller/order.controller.js
@@ -1,4 +1,5 @@
 const orderModel = require('../model/order.model');
+const productModel = require ('../model/product.model')
 const {failed, success}= require('../helper/response');
 
 const orderController = {
@@ -36,6 +37,17 @@ const orderController = {
         failed(res, err.message, 'failed', `failed to get order detail`);
     })
   },
+  detailUserOrder: (req, res) => {
+    const id = req.params.id;
+
+    orderModel.selectUserOrder(id)
+    .then((result) => {
+        success(res, result.rows, 'success', `get detail order success`);
+    })
+    .catch((err) => {
+        failed(res, err.message, 'failed', `failed to get order detail`);
+    })
+  },
   insert: (req, res) => {
     try {
       const {userid, item, quantity, color, size, status} = req.body;
@@ -65,14 +77,58 @@ const orderController = {
     const { quantity,color,size,status} = req.body
     const data = {quantity,color,size,status,id}
     orderModel
-      .updateProduct(data)
+      .updateOrder(data)
       .then((result) => {
         success(res, result,data, 'success', 'upload orders success')
       })
       .catch((err) => {
         failed(res, err.message, 'failed', 'internal server error');
       })
-  }
+  },
+  insertTransaction: (req, res) => {
+    try {
+      const {userid, id_order, payment_method,quantity,price,id,stockProduk,StockOrder} = req.body;
+      const total_price=quantity*price
+      const data = {
+        userid,
+        id_order,
+        payment_method,
+        total_price
+      }
+      orderModel.storeTransaksi(data)
+      .then((result) => {
+          const stock = stockProduk-StockOrder
+          const data1= {stock,id}
+          productModel.updateProduct(data1)
+          success(res, result, 'success', 'Update stock success')
+      }).catch((err) => {
+          failed(res, err.message, 'failed', 'insert transaksi failed')
+      })
+    } catch(err) {
+      failed(res, err.message, 'failed', 'internal server error');
+    }
+  },
+  listTransaksi: (req, res) => {
+    orderModel
+      .selectAllTransaction()
+      .then((result) => {
+        success(res, result, 'success','get all orders succes')
+      })
+      .catch((err) => {
+        failed(res, err.message,'failed','get all orders failed')
+      })
+  },
+  listTransaksiID: (req, res) => {
+    const id = req.params.id
+    orderModel
+      .selectTransactionID(id)
+      .then((result) => {
+        success(res, result, 'success','get all orders succes')
+      })
+      .catch((err) => {
+        failed(res, err.message,'failed','get all orders failed')
+      })
+  },
 }
 
 module.exports = orderController
