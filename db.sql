4ab6095c20a9a55cf966e53d6caf0bcc5207d1a3
IchsanR <111603307+IchsanR@users.noreply.github.com>
2022-11-27 21:56:45 +0800

    ...

--- db.sql
+++ db.sql
@@ -41,7 +41,7 @@ create table orders(
     item integer references product(id_product) on delete cascade,
     quantity integer,
     item_color varchar(20),
-    item_size integer, -- 1 XS, 2 S, 3 M, 4 L, 5 XL
+    item_size text, -- 1 XS, 2 S, 3 M, 4 L, 5 XL
     status integer -- 0 unpaid, 1 paid, 2 packed, 3 sent, 4 complete, 5 canceled
 );
 
